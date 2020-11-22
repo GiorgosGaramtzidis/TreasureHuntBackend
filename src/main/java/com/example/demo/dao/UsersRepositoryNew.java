3020a119660395a8042c86ef94bd5787e5e0861f
Giorgios Garamtzidis <giorgosss@live.se>
2020-11-22 17:27:14 +0200

    Closed issue #27 : Users Service added

--- src/main/java/com/example/demo/dao/UsersRepositoryNew.java
+++ src/main/java/com/example/demo/dao/UsersRepositoryNew.java
@@ -1,4 +1,8 @@
 package com.example.demo.dao;
+import com.example.demo.model.UsersNew;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UsersRepositoryNew {
+@Repository
+public interface UsersRepositoryNew extends MongoRepository<UsersNew,String> {
 }
