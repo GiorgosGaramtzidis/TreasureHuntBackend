6a61ccc5ffbffbf33b7fc69faab0fb57455af3f2
ChrysoZalo <chrysozalo@gmail.com>
2020-12-02 21:29:31 +0200

    remove no used classes

--- src/main/java/com/example/demo/dao/MongoQuestionsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.model.QuestionsNew;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-
-public interface MongoQuestionsRepository extends MongoRepository<QuestionsNew , String> {
-}
