9949cca0b7bf0d305fc5008e9a7c43df347e4841
AthinaVrettea <Athina_V@hotmail.gr>
2020-12-06 18:21:25 +0200

    Commit Issue #34: Adding lose condition service.

--- src/main/java/com/example/demo/Service/LoseConditionService.java
+++ src/main/java/com/example/demo/Service/LoseConditionService.java
@@ -10,22 +10,30 @@ import com.example.demo.model.Users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class LoseConditionService implements LoseConditionRegistration<Users> {
 
     @Autowired
     UsersRepository usersRepository;
     @Override
-    public Integer loseCondition(Users user) throws Exception {
-        if(usersRepository.existsById(user.getId())){
-        if(user.getUserLives()<=1){
-            throw new Exception("Out of lives");
+    public Integer loseCondition(String userName) throws Exception {
+        if(usersRepository.existsByUserName(userName))
+        {
+            List<Users> user =usersRepository.findAll().stream().filter(users ->users.getUserName().equals(userName))
+                    .collect(Collectors.toList());
+
+                if(user.get(0).getUserLives()<=1){
+                     throw new Exception("Out of lives");
+                } else{
+                     user.get(0).setUserLives(user.get(0).getUserLives()-1);
+                     usersRepository.save(user.get(0));
+                     return user.get(0).getUserLives();
+                }
+        }else {
+            throw new Exception("Wrong id");
         }
-        else{
-            user.setUserLives(user.getUserLives()-1);
-            usersRepository.save(user);
-            return user.getUserLives();
-        }};
-        return 1;
     }
 }
