5cefe835cbda66db9502e39ff2af97f36d3bb487
thaliaxitziou <thaliaxitziou@gmail.com>
2020-12-12 19:23:17 +0200

    start score from 0 start lives from 5

--- src/test/java/com/example/demo/Service/UsersServiceTest.java
+++ src/test/java/com/example/demo/Service/UsersServiceTest.java
@@ -2,16 +2,19 @@ package com.example.demo.Service;
 
 import com.example.demo.dao.UsersRepository;
 import com.example.demo.model.User;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
@@ -56,6 +59,32 @@ public class UsersServiceTest {
         when(usersRepository.findUserByUserName("blah")).thenReturn(user);
 
         Boolean actualResult = usersService.loginConfirmation("blah", "foo");
+        assertEquals(true,actualResult);
+    }
+
+    @Test(expected = Exception.class)
+    public void restartScoreAndLivesWhenUserDoesNotExistShouldCreateException() throws Exception {
+
+        when(usersRepository.existsByUserName("Konto4")).thenReturn(false);
+        usersService.restartScoreAndLives("Konto4");
+        fail("This should return User does not exist");
+    }
+
+    @Test
+    public void restartScoreAndLivesWhenUserDoesExistShouldReturnFalse() throws Exception {
+
+        List<User> users = new ArrayList<>();
+        User user = new User();
+        user.setUserName("blah");
+        users.add(user);
+
+        when(usersRepository.existsByUserName(user.getUserName())).thenReturn(true);
+        when(usersRepository.findAll().stream()
+                .filter(user1 -> user1.getUserName().equals(user.getUserName()))
+                .collect(Collectors.toList())).thenReturn(users);
+
+
+        Boolean actualResult = usersService.restartScoreAndLives("blah");
         assertEquals(true,actualResult);
     }
 }
