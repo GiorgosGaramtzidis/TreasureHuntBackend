ebca2414379211c7422e9467bc658d19d7f7e2fe
Giorgios Garamtzidis <giorgosss@live.se>
2021-01-17 12:50:05 +0200

    Corrected some tests

--- src/test/java/com/example/demo/Service/AnswerCheckServiceTest.java
+++ src/test/java/com/example/demo/Service/AnswerCheckServiceTest.java
@@ -1,22 +1,11 @@
 package com.example.demo.Service;
 
-import com.example.demo.dao.QuestionsRepository;
-import com.example.demo.model.Question;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.MockBeans;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,30 +14,29 @@ public class AnswerCheckServiceTest {
     @Autowired
     AnswerCheckService answerCheckService;
 
-
     @Test
-    public void CheckingTheAnswerIfItsTheCorrectOneWhileLocationTitleExists() throws Exception {
+    public void CheckingTheAnswerIfItsTheCorrectOneWhileQuestionExists() throws Exception {
         String usersAnswer = "Κουτάλι";
-        String locationTitle = "Όλους ταΐζει και δεν τρώει. Τι είναι;";
-        assertEquals(true,answerCheckService.AnswerCheck(usersAnswer,locationTitle));
+        String question = "Όλους ταΐζει και δεν τρώει. Τι είναι;";
+        assertEquals(true,answerCheckService.AnswerCheck(usersAnswer,question));
     }
 
     @Test
-    public void CheckingTheAnswerIfItsWrongWhileLocationTitleExists() throws Exception {
-        String usersAnswer = "wrong";
-        String locationTitle = "Όλους ταΐζει και δεν τρώει. Τι είναι;";
-        assertEquals(false,answerCheckService.AnswerCheck(usersAnswer,locationTitle));
+    public void CheckingTheAnswerIfItsWrongWhileQuestionExists() throws Exception {
+        String usersAnswer = "foo";
+        String question = "Όλους ταΐζει και δεν τρώει. Τι είναι;";
+        assertEquals(false,answerCheckService.AnswerCheck(usersAnswer,question));
     }
     @Test(expected = Exception.class)
-    public void CheckingTheAnswerWhileLocationTitleDoesntExistsAndAnswerIsWrong() throws Exception{
+    public void CheckingTheAnswerWhileQuestionDoesntExistsAndAnswerIsWrong() throws Exception{
         String usersAnswer = "dfsgdah";
-        String locationTitle = "GFDSGFD";
-        assertEquals(false,answerCheckService.AnswerCheck(usersAnswer,locationTitle));
+        String question = "GFDSGFD";
+        assertEquals(false,answerCheckService.AnswerCheck(usersAnswer,question));
     }
     @Test(expected = Exception.class)
-    public void CheckingTheAnswerWhileLocationTitleDoesntExistsAndAnswerIsCorrect() throws Exception{
+    public void CheckingTheAnswerWhileQuestionDoesntExistsAndAnswerIsCorrect() throws Exception{
         String usersAnswer = "Κουτάλι";
-        String locationTitle = "GFDSGFD";
-        assertEquals(false,answerCheckService.AnswerCheck(usersAnswer,locationTitle));
+        String question = "GFDSGFD";
+        assertEquals(false,answerCheckService.AnswerCheck(usersAnswer,question));
     }
 }
